6ad5002b4433efe99352b1ea5d237402d8da4619
Pawel Zabczynski <pawelzabczynski@pawelzabczynski.com>
2020-03-14 23:54:09 +0100

    add basic project structure

--- src/main/scala/com/pawelzabczynski/covid/DataRepository.scala
+++ src/main/scala/com/pawelzabczynski/covid/DataRepository.scala
@@ -1,14 +1,14 @@
 package com.pawelzabczynski.covid
 
 import com.pawelzabczynski.config.DataSourceConfig
-import com.pawelzabczynski.covid.model.{CovidObservation, Data, Model}
+import com.pawelzabczynski.covid.model.{CovidObservation, DataSourceDescriber, Model}
 import org.apache.spark.sql.{Dataset, SparkSession}
 
 import scala.reflect.runtime.universe.TypeTag
 
 class DataRepository(spark: SparkSession, dataSourceConf: DataSourceConfig) {
 
-  def load[T <: Model[T]](data: Data[T])(implicit typeTag: TypeTag[T]): Dataset[T] = {
+  def load[T <: Model[T]](data: DataSourceDescriber[T])(implicit typeTag: TypeTag[T]): Dataset[T] = {
     import spark.implicits._
 
     spark
@@ -30,6 +30,6 @@ object DataRepository {
   val CSV = "csv"
   val NAMESPACE = "COVID"
 
-  val COVID_OBSERVATION_DS: Data[CovidObservation] = Data[CovidObservation]("covid_19_data.csv", NAMESPACE, "")
+  val COVID_OBSERVATION_DS: DataSourceDescriber[CovidObservation] = DataSourceDescriber[CovidObservation]("covid_19_data.csv", NAMESPACE, "")
 
 }
--- src/main/scala/com/pawelzabczynski/covid/model/DataSourceDescriber.scala
+++ src/main/scala/com/pawelzabczynski/covid/model/DataSourceDescriber.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.types.StructType
 
 import scala.reflect.runtime.universe.TypeTag
 
-case class Data[T <: Model[T]](fileName: String, namespace: String, tableName: String) {
+case class DataSourceDescriber[T <: Model[T]](fileName: String, namespace: String, tableName: String) {
   def sourceFile(dataDir: String): String = s"$dataDir/$fileName"
   def schema()(implicit typeTag: TypeTag[T]): StructType = Model.schema[T]
   def sqlSchema()(implicit typeTag: TypeTag[T]): StructType = Model.sqlSchema[T]
